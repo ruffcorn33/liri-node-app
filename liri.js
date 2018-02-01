08e5b1e1a5540c7a7e3497ee823b1a0a8659ba3c
Craig Christensen <ruffcorn@gmail.com>
2018-02-01 16:20:25 -0600

    error handling

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 //var keys = require('keys.js');
 var fs = require('fs');
 var request = require('request');
+var Twitter = require('twitter');
+
 
 // LIRI flow control
 var operation = process.argv[2]
@@ -23,40 +25,43 @@ switch(operation){
     doHelp();
     break;
   default:
-    console.log("Try 'node liri help'");   
+    doError(3);  
 }
 
 function doTweets(){
-  console.log('do tweets stuff here');
+  console.log('Twitter functionality is under construction');
+  var client = new Twitter({
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+  });
 }
 
 function doSpotify(){
-  console.log('do spotify stuff here');
+  // throw error for no song title
+  if (process.argv.length < 4){doError(1);};
+  console.log('Spotify functionality is under construction');
 }
 
 function doMovie(){
+  // throw error for no movie title
+  if (process.argv.length < 4){doError(2)};
   // assemble movie name
   var movieName = "";
   for (i=3; i<process.argv.length; i++){
     movieName = movieName + ' ' + process.argv[i];
-  }
-  // trim leading whitespace
-  movieName = movieName.trim();
-  // replace internal spaces with '+'
-  movieName = movieName.replace(" ", "+");
-  
-  
-  // Assemble request URL
+  };
+  // trim leading whitespace and replace internal spaces with '+'
+  movieName = movieName.trim().replace(" ", "+");
+  // assemble request URL
   var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
   // create a request to the queryUrl
-  var movieObj;
   request.get(queryUrl, function(err, res, body){
-    movieObj = JSON.parse(body);
-    // console.log(movieObj);
-
-    // loop through the Ratings array of objects  
+    var movieObj = JSON.parse(body); // console.log(movieObj);
     var imdb_rating;
     var rotTomatoes;
+    // loop through the Ratings array of objects  
     for (i=0; i<movieObj.Ratings.length; i++){
       if (movieObj.Ratings[i].Source === 'Internet Movie Database') {
         imdb_rating = movieObj.Ratings[i].Value;
@@ -65,7 +70,6 @@ function doMovie(){
         rotTomatoes = movieObj.Ratings[i].Value;
       };
     };
-
     // output movie data to console
     console.log('Title: '+movieObj.Title);
     console.log('Year: '+movieObj.Year);
@@ -80,9 +84,12 @@ function doMovie(){
     if (movieObj.Plot != 'N/A'){
       console.log('Plot: '+movieObj.Plot);
     };
-    console.log('Actors: '+movieObj.Actors);
-    
+    console.log('Actors: '+movieObj.Actors); 
   });
+}
+
+function doRandom(){
+  console.log('random functionality is under construction');
 }
   
 function doHelp(){
@@ -92,4 +99,19 @@ function doHelp(){
   console.log('  spotify-this-song          [song title]');
   console.log('  movie-this                 [movie title]');
   console.log('  do-what-it-says            [no arguments]');
+}
+
+function doError(err){
+  switch(err){
+    case 1:
+      console.log('You must include a song title');
+      break;
+    case 2:
+      console.log('You must include a movie title');
+      break;
+    case 3:
+      console.log("Try 'node liri help'");
+    default:
+      console.log('Unknown error');
+  };
 }
